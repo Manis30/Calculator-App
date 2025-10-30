4310743f6f20c7a642070e1ca2d1d0ad87346f7e
Mani <mani30saravanan@gmail.com>
2025-10-30 18:31:50 +0530

    Mobile Responsive is done

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -75,47 +75,47 @@ const calculate=()=>{
   }
     return(
         <div className="bg-gradient-to-r from-blue-500 to-purple-600 h-screen w-full flex justify-center items-center">
-            <div className="bg-white w-130 rounded p-[30px]">
+            <div className="bg-white w-80 sm:w-130 rounded p-[30px]">
                 <div className="text-right  mt-4 pr-[20px] break-words">
                     <h3 className="text-[35px] font-bold ">{result=='NaN'?"Invalid Expression":result}</h3>
                 </div>
                 <div className="mt-10">
                     <div className="grid grid-cols-4 mb-5">
-                        <div className="flex justify-center"><button type="button" className="bg-[#f64141] h-[70px] w-[80px] rounded text-[23px] hover:bg-[#fe0000]" onClick={clearExpression}>C</button></div>
-                        <div className="flex justify-center"><button type="button" className="bg-[#c1c1c1fc] h-[70px] w-[80px] rounded text-[23px] hover:bg-[#989696]" onClick={handlePercentage}>%</button></div>
-                        <div className="flex justify-center"><button type="button" className="bg-[#c1c1c1fc] h-[70px] w-[80px] rounded text-[23px] hover:bg-[#989696]" onClick={toggleSign}>±</button></div>
-                        <div className="flex justify-center"><button type="button" className="bg-[#c1c1c1fc] h-[70px] w-[80px] rounded text-[23px] hover:bg-[#989696]" onClick={removeSpace}>⌫</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#f64141] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[23px] hover:bg-[#fe0000] " onClick={clearExpression}>C</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#c1c1c1fc] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[23px] hover:bg-[#989696]" onClick={handlePercentage}>%</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#c1c1c1fc] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[23px] hover:bg-[#989696]" onClick={toggleSign}>±</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#c1c1c1fc] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[23px] hover:bg-[#989696]" onClick={removeSpace}>⌫</button></div>
                     </div>
                     <div className="grid grid-cols-4 mb-5">
-                        <div className="flex justify-center"><button type="button" className="bg-[#c1c1c1fc] h-[70px] w-[80px] rounded text-[23px] hover:bg-[#989696]"  onClick={divideByOne} >1/x</button></div>
-                        <div className="flex justify-center"><button type="button" className="bg-[#c1c1c1fc] h-[70px] w-[80px] rounded text-[23px] hover:bg-[#989696]" onClick={square}>x²</button></div>
-                        <div className="flex justify-center"><button type="button" className="bg-[#c1c1c1fc] h-[70px] w-[80px] rounded text-[23px] hover:bg-[#989696]" onClick={squareRoot}>√x</button></div>
-                        <div className="flex justify-center"><button type="button" className="bg-[#f57c09] h-[70px] w-[80px] rounded text-[30px] hover:bg-[#ff4400]" onClick={()=>handleInput('÷')}>÷</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#c1c1c1fc] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[23px] hover:bg-[#989696]"  onClick={divideByOne} >1/x</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#c1c1c1fc] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[23px] hover:bg-[#989696]" onClick={square}>x²</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#c1c1c1fc] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[23px] hover:bg-[#989696]" onClick={squareRoot}>√x</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#f57c09] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[30px] hover:bg-[#ff4400]" onClick={()=>handleInput('÷')}>÷</button></div>
                     </div>
                     <div className="grid grid-cols-4 mb-5">
-                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[70px] w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]"  onClick={()=>handleInput(7)} >7</button></div>
-                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[70px] w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput(8)}>8</button></div>
-                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[70px] w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput(9)}>9</button></div>
-                        <div className="flex justify-center"><button type="button" className="bg-[#f57c09] h-[70px] w-[80px] rounded text-[30px] hover:bg-[#ff4400]" onClick={()=>handleInput('×')}>×</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]"  onClick={()=>handleInput(7)} >7</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput(8)}>8</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput(9)}>9</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#f57c09] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[30px] hover:bg-[#ff4400]" onClick={()=>handleInput('×')}>×</button></div>
                     </div>
                     <div className="grid grid-cols-4 mb-5">
-                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[70px] w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput(4)}>4</button></div>
-                        <div className="flex justify-center" ><button type="button" className="bg-[#d0c8c8] h-[70px] w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput(5)}>5</button></div>
-                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[70px] w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput(6)}>6</button></div>
-                        <div className="flex justify-center"><button type="button" className="bg-[#f57c09] h-[70px] w-[80px] rounded text-[30px] hover:bg-[#ff4400]" onClick={()=>handleInput('-')}>-</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput(4)}>4</button></div>
+                        <div className="flex justify-center" ><button type="button" className="bg-[#d0c8c8] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput(5)}>5</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput(6)}>6</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#f57c09] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[30px] hover:bg-[#ff4400]" onClick={()=>handleInput('-')}>-</button></div>
                     </div>
                     <div className="grid grid-cols-4 mb-5">
-                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[70px] w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput(1)}>1</button></div>
-                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[70px] w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput(2)}>2</button></div>
-                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[70px] w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput(3)}>3</button></div>
-                         <div className="flex justify-center"><button type="button" className="bg-[#f57c09] h-[70px] w-[80px] rounded text-[30px] hover:bg-[#ff4400] "  onClick={()=>handleInput('+')}>+</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput(1)}>1</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput(2)}>2</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput(3)}>3</button></div>
+                         <div className="flex justify-center"><button type="button" className="bg-[#f57c09] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[30px] hover:bg-[#ff4400] "  onClick={()=>handleInput('+')}>+</button></div>
                     </div> 
                     
                     <div className="grid grid-cols-4 mb-5">
-                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[70px] w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput('00')}>00</button></div>
-                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[70px] w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput(0)}>0</button></div>
-                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[70px] w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput('.')}>.</button></div>
-                        <div className="flex justify-center"><button type="button" className="bg-[#427df1] text-white h-[70px] w-[80px] rounded text-[35px] hover:bg-[#0054f9]" onClick={calculate}>=</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput('00')}>00</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput(0)}>0</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#d0c8c8] h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[23px] hover:bg-[#dbd6d6]" onClick={()=>handleInput('.')}>.</button></div>
+                        <div className="flex justify-center"><button type="button" className="bg-[#427df1] text-white h-[55px] w-[55px] text-[20px] sm:h-[70px] sm:w-[80px] rounded text-[35px] hover:bg-[#0054f9]" onClick={calculate}>=</button></div>
                     </div>  
                 </div>
                 <div>
